bcbd6e0cc1e7cf108b3bb9e9c13ba109f792bf71
Shirshendu Mahata <shirshendumahata@gmail.com>
2023-10-12 23:53:55 +0530

    up

--- src/Pages/Home/Bennar.js
+++ src/Pages/Home/Bennar.js
@@ -3,23 +3,21 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
 var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
 
-
-export default function Bennar (){
-    return(
-        <>
-         <div className="container-fluid mb-3">
-    <div className="row px-xl-5">
-      <div className="col-lg-8">
-        {/* <div id="header-carousel" className="carousel slide carousel-fade mb-30 mb-lg-0" data-ride="carousel">
+export default function Bennar() {
+  return (
+    <>
+      <div className="container-fluid mb-3">
+        <div className="row px-xl-5">
+          <div className="col-lg-8">
+            {/* <div id="header-carousel" className="carousel slide carousel-fade mb-30 mb-lg-0" data-ride="carousel">
           <ol className="carousel-indicators">
             <li data-target="#header-carousel" data-slide-to={0} className="active" />
             <li data-target="#header-carousel" data-slide-to={1} />
@@ -28,62 +26,110 @@ export default function Bennar (){
           <div className="carousel-inner"> */}
 
             <Slider {...settings}>
-            
- <div className=" position-relative active" style={{height: 430}}>
-              <img className="position-absolute w-100 h-100" src="img/carousel-1.jpg" style={{objectFit: 'cover'}} />
-              <div className=" d-flex flex-column align-items-center justify-content-center">
-                <div className="p-3" style={{maxWidth: 700}}>
-                  <h1 className="display-4 text-white mb-3 animate__animated animate__fadeInDown">Men Fashion</h1>
-                  <p className="mx-md-5 px-5 animate__animated animate__bounceIn">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum diam</p>
-                  <a className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Shop Now</a>
+              <div
+                className=" position-relative active"
+                style={{ height: '150vh' }}
+              >
+                <img
+                  className="position-absolute w-100 h-100"
+                  src="img/carousel-1.jpg"
+                  style={{ objectFit: "cover", }}
+                />
+                <div className=" d-flex flex-column align-items-center justify-content-center">
+                  <div className="p-3" style={{ maxWidth: 700 }}>
+                    <h1 className="display-4 text-white mb-3 animate__animated animate__fadeInDown">
+                      Men Fashion
+                    </h1>
+                    <p className="mx-md-5 px-5 animate__animated animate__bounceIn">
+                      Lorem rebum magna amet lorem magna erat diam stet. Sadips
+                      duo stet amet amet ndiam elitr ipsum diam
+                    </p>
+                    <a
+                      className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp"
+                      href="#"
+                    >
+                      Shop Now
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className=" position-relative" style={{height: 430}}>
-              <img className="position-absolute w-100 h-100" src="img/carousel-2.jpg" style={{objectFit: 'cover'}} />
-              <div className=" d-flex flex-column align-items-center justify-content-center">
-                <div className="p-3" style={{maxWidth: 700}}>
-                  <h1 className="display-4 text-white mb-3 animate__animated animate__fadeInDown">Women Fashion</h1>
-                  <p className="mx-md-5 px-5 animate__animated animate__bounceIn">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum diam</p>
-                  <a className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Shop Now</a>
+              <div className=" position-relative" style={{ height: "430px" }}>
+                <img
+                  className="position-absolute w-100 h-100"
+                  src="img/carousel-2.jpg"
+                  style={{ objectFit: "cover" }}
+                />
+                <div className=" d-flex flex-column align-items-center justify-content-center">
+                  <div className="p-3" style={{ maxWidth: 700 }}>
+                    <h1 className="display-4 text-white mb-3 animate__animated animate__fadeInDown">
+                      Women Fashion
+                    </h1>
+                    <p className="mx-md-5 px-5 animate__animated animate__bounceIn">
+                      Lorem rebum magna amet lorem magna erat diam stet. Sadips
+                      duo stet amet amet ndiam elitr ipsum diam
+                    </p>
+                    <a
+                      className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp"
+                      href="#"
+                    >
+                      Shop Now
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className=" position-relative" style={{height: 430}}>
-              <img className="position-absolute w-100 h-100" src="img/carousel-3.jpg" style={{objectFit: 'cover'}} />
-              <div className=" d-flex flex-column align-items-center justify-content-center">
-                <div className="p-3" style={{maxWidth: 700}}>
-                  <h1 className="display-4 text-white mb-3 animate__animated animate__fadeInDown">Kids Fashion</h1>
-                  <p className="mx-md-5 px-5 animate__animated animate__bounceIn">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet amet amet ndiam elitr ipsum diam</p>
-                  <a className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Shop Now</a>
+              <div className=" position-relative" style={{ height: "430px" }}>
+                <img
+                  className="position-absolute w-100 h-100"
+                  src="img/carousel-3.jpg"
+                  style={{ objectFit: "cover" }}
+                />
+                <div className=" d-flex flex-column align-items-center justify-content-center">
+                  <div className="p-3" style={{ maxWidth: 700 }}>
+                    <h1 className="display-4 text-white mb-3 animate__animated animate__fadeInDown">
+                      Kids Fashion
+                    </h1>
+                    <p className="mx-md-5 px-5 animate__animated animate__bounceIn">
+                      Lorem rebum magna amet lorem magna erat diam stet. Sadips
+                      duo stet amet amet ndiam elitr ipsum diam
+                    </p>
+                    <a
+                      className="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp"
+                      href="#"
+                    >
+                      Shop Now
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
             </Slider>
-           
-          {/* </div>
+
+            {/* </div>
         </div> */}
-      </div>
-      <div className="col-lg-4">
-        <div className="product-offer mb-30" style={{height: 200}}>
-          <img className="img-fluid" src="img/offer-1.jpg" alt />
-          <div className="offer-text">
-            <h6 className="text-white text-uppercase">Save 20%</h6>
-            <h3 className="text-white mb-3">Special Offer</h3>
-            <a href className="btn btn-primary">Shop Now</a>
+          </div>
+          <div className="col-lg-4">
+            <div className="product-offer mb-30" style={{ height: 200 }}>
+              <img className="img-fluid" src="img/offer-1.jpg" alt />
+              <div className="offer-text">
+                <h6 className="text-white text-uppercase">Save 20%</h6>
+                <h3 className="text-white mb-3">Special Offer</h3>
+                <a href className="btn btn-primary">
+                  Shop Now
+                </a>
+              </div>
+            </div>
+            <div className="product-offer mb-30" style={{ height: 200 }}>
+              <img className="img-fluid" src="img/offer-2.jpg" alt />
+              <div className="offer-text">
+                <h6 className="text-white text-uppercase">Save 20%</h6>
+                <h3 className="text-white mb-3">Special Offer</h3>
+                <a href className="btn btn-primary">
+                  Shop Now
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="product-offer mb-30" style={{height: 200}}>
-          <img className="img-fluid" src="img/offer-2.jpg" alt />
-          <div className="offer-text">
-            <h6 className="text-white text-uppercase">Save 20%</h6>
-            <h3 className="text-white mb-3">Special Offer</h3>
-            <a href className="btn btn-primary">Shop Now</a>
-          </div>
-        </div>
       </div>
-    </div>
-  </div>
-        </>
-    )
+    </>
+  );
 }
